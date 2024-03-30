883af02d1ab7ba1d7232fb93136501064789409c
Luca Scheller <luca.scheller@gmail.com>
2024-03-29 18:16:56 -0700

    Add asset gallery mock data

--- package/husdplugins/datasources/library.py
+++ package/husdplugins/datasources/library.py
@@ -1,6 +1,9 @@
+import datetime
 import json
 import os
-from dataclasses import dataclass
+import time
+from dataclasses import dataclass, field
+from typing import Any
 
 import hou
 import husd
@@ -17,49 +20,173 @@ class ItemColor:
 
 @dataclass
 class Item:
+    id: str
     # Hierarchy
-    parent = ""
-    children = ""
+    parent: str = ""
+    children: dict = field(default_factory = lambda: ({}))
     # Data
-    label = ""
-    file_path = ""
-    thumbnail_file_path = ""
-    creation_date = 0
-    modification_date = 0
-    meta_data = {}
-    blind_data = b""
+    label: str = ""
+    file_path: str = ""
+    thumbnail_file_path: str = ""
+    thumbnail_file_content: bytes = b"" 
+    creation_date: int = 0
+    modification_date: int = 0
+    meta_data: dict = field(default_factory = lambda: ({}))
+    blind_data: str = ""
     # UI
-    tags = []
-    color = ""
-    star = False
+    tags: list[str] = field(default_factory = lambda: ([]))
+    color: str = ""
+    star: bool = False
 
 
 class Model(object):
-    def __init__(self) -> None:
+    def __init__(self, identifier: str) -> None:
+        """Initialize the model.
+        Args:
+            identifier(str): The identifier.
+        """
         # Static
-        self.default_thumbnail_file_path = ""
+        self.identifier = identifier
 
         # Items
         self.items = {}
+        self.items_updated = set()
         self.items_deleted = {}
 
         # UI
         self.tags = []
 
-    def get_data(self, item_id, role):
+    def get_tags(self) -> tuple[str]:
+        """Get all tags.
+        Returns
+            list[str]: The tags.
+        """
+        return tuple(self.tags)
+
+    def add_tag(self, tag:str) -> bool:
+        """Create a tag, return True if 
+        the tag did not already exist and was created.
+
+        Args:
+            tag(str): The tag name.
+        Returns:
+            str: The success state.
+        """
+        if tag not in self.tags:
+            self.tags.append(tag)
+            return True
+        return False
+
+    def delete_tag(self, tag:str, delete_if_assigned: bool) -> bool:
+        """Delete a tag, return True if 
+        the tag did not already exist and was created.
+
+        Args:
+            tag(str): The tag name.
+        Returns:
+            str: The success state.
+        """
+        tag_assigned = False
+        for item in self.items.values():
+            if tag in item.tags:
+                tag_assigned = True
+                break
+        if not delete_if_assigned and tag_assigned:
+            return False
+        
+        if tag_assigned:
+            for item in self.items.values():
+                if tag in item.tags:
+                    item.tags.remove(item)
+            
+        tag_exists = tag in self.tags
+        if tag_exists:
+            self.tags.remove(tag)
+        return tag_assigned or tag_exists
+
+    def get_item_ids(self) -> tuple[str]:
+        """Get the ids of all items.
+
+        Returns:
+            tuple[str]: The item ids.
+        """
+        return tuple(self.items.keys())
+    
+    def get_item_updated_ids(self) -> tuple[str]:
+        """Get the ids of all updated items.
+        This clears the update queue.
+
+        Returns:
+            tuple[str]: The item ids.
+        """
+        ids = tuple(self.items_updated)
+        self.items_updated.clear()
+        return ids
+
+    def set_item_mark_deleted(self, item_id: str) -> bool:
+        """Marks the item to be deleted.
+        Args:
+            item_id(str): The item id to mark as deleted.
+        Returns:
+            str: The success state.
+        """
+        item = self.items.pop(item_id, None)
+        if item:
+            self.items_deleted[item_id] = item
+            return True
+        return False
+
+    def set_item_mark_undeleted(self, item_id: str) -> bool:
+        """Un-mark the item to be deleted.
+        Args:
+            item_id(str): The item id to restore.
+        Returns:
+            str: The success state.
+        """
+        item = self.items_deleted.pop(item_id, None)
+        if item:
+            self.items[item_id] = item
+            return True
+        return False
+
+    def add_item(self, item: Item):
+        """Add an item to the model.
+        Args:
+            item(Item): The item to add.
+        """
+        self.items[item.id] = item
+        for tag in item.tags:
+            self.add_tag(tag)
+
+    def get_data(self, item_id: str, role: Any):
+        """Get data with the given role from the item.
+        Args:
+            item_id(str): The item id.
+            role(str): The data role.
+        Returns:
+            Any: The data.
+        """
         item = self.items.get(item_id)
         data = getattr(item, role, None)
-        if role == 'thumbnail' and data is None:
+        if role == 'thumbnail_file_content' and not data:
             thumbnail_file_path = getattr(item, 'thumbnail_file_path', None)
             data = b''
             if thumbnail_file_path and os.path.isfile(thumbnail_file_path):
                 with open(thumbnail_file_path, "r") as thumbnail_file:
                     data = thumbnail_file.read()
-                setattr(item, 'thumbnail', data)
+                setattr(item, 'thumbnail_file_content', data)
 
         return data
 
-    def set_data(self, item_id, role, value):
+    def set_data(self, item_id: str, role: str, value: Any) -> bool:
+        """Set data with the given role on the given item.
+        Args:
+            item_id(str): The item id.
+            role(str): The data role.
+            value(Any): The data.
+        Returns:
+            bool: The success state.
+        """
         item = self.items.get(item_id)
         if not item:
             return False
@@ -67,14 +194,68 @@ class Model(object):
         if data == value:
             return False
         else:
+            self.items_updated.add(item_id)
             setattr(item, role, value)
             return True
 
-    def load(self):
-        pass
+    def load(self, identifier: str=None):
+        """Load the model from the given identifier.
+        Args:
+            identifier(str): The model id.
+        """
+        if not identifier:
+            identifier = self.identifier
+        if identifier == "__mock__.env":
+            populate_mock_data(self)
+            self.save("MOCK.env")
+        else:
+            env_name = identifier.replace(".env", "")
+            data = hou.getenv(env_name, "")
+            if not data:
+                return
+            data = json.loads(data)
+            self.identifier = data["identifier"]
+            self.tags = data["tags"]
+            for item_id, item_data in data["items"].items():
+                self.items[item_id] = Item(item_id, **item_data)
 
-    def save(self):
-        pass
+    def save(self, identifier: str=None):
+        """Save the model to its identifier."""
+        if not identifier:
+            identifier = self.identifier
+        env_name = identifier.replace(".env", "")
+        data = {"identifier": self.identifier, "items": {}, "tags": self.tags}
+        item: Item
+        for item in self.items.values():
+            item_data = item.__dict__
+            item_data = {k:v for k,v in item_data.items()}
+            item_data.pop('thumbnail_file_content')
+            data["items"][item.id] = item_data
+        data = json.dumps(data)
+        hou.hscript('setenv {env_name}="{data}"'.format(env_name=env_name,
+                                                        data=data))
+
+
+def populate_mock_data(model: Model):
+    self = model
+    for idx in range(5):
+        self.add_tag(f"Tag {idx}")
+    item_colors = [ItemColor.red, ItemColor.green, ItemColor.blue]
+    for idx in range(10):
+        item_creation_date = time.mktime(datetime.datetime.now().timetuple())
+        item = Item(
+            f"id_{idx}",
+            label=f"Item Label {idx}",
+            file_path=f"/some/path/{idx}.usd",
+            thumbnail_file_path=f"/some/path/{idx}.png",
+            creation_date=item_creation_date,
+            modification_date=item_creation_date,
+            meta_data={"keyA": "valueA", "keyB": "valueB"},
+            tags=["Tag 0", f"Item Tag {idx}"],
+            color=item_colors[idx % 3],
+            star=idx % 3 == 0
+        )
+        self.add_item(item)
 
 
 class AssetLibrary(husd.datasource.DataSource):
@@ -82,10 +263,13 @@ class AssetLibrary(husd.datasource.DataSource):
 
     def __init__(self, source_identifier, args=None):
         self.source_identifier = source_identifier
-        self.source_args = args
+        self.source_args = args # Not implemented for now.
 
         # Storage
-        self.model = Model()
+        self.model = Model(source_identifier)
+
+        # Init
+        self.model.load()
 
     def isValid(self) -> bool:
         """The data source state.
@@ -99,8 +283,19 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             bool: Data source read only state.
         """
-        return True
+        return False
 
+    @staticmethod
+    def canHandle(source_identifier):
+        """Check if this model can handle the specified
+        model identifier.
+        Args:
+            identifier(str): The model identifier.
+        Returns:
+            bool: The state.
+        """
+        return source_identifier.endswith(".env")
+    
     def sourceIdentifier(self) -> str:
         """The source identifier string used to create this data source object.
         Returns:
@@ -134,11 +329,13 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The success state.
         """
+        return self.model.save(source_identifier)
 
     def startTransaction(self):
         """For writable data sources, this method is used to group
         multiple calls to edit the data source.
         """
+        pass
 
     def endTransaction(self, commit=True):
         """This method is always called after a call to startTransaction,
@@ -146,12 +343,15 @@ class AssetLibrary(husd.datasource.DataSource):
         Args:
             commit (bool): If True, make the changes persistent.
         """
+        if commit:
+            self.model.save()
 
     def itemIds(self) -> tuple[str]:
         """A unique identifier for each asset available in the data source.
         Returns:
             list[str]: The item identifiers.
         """
+        return self.model.get_item_ids()
 
     def updatedItemIds(self) -> tuple[str]:
         """Return a unique identifier for any asset that has changed since
@@ -163,6 +363,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             tuple[str]: Item identifiers of items that have data changes.
         """
+        return self.model.get_item_updated_ids()
 
     def parentId(self, item_id: str) -> str:
         """The unique identifier for this item’s parent item.
@@ -171,6 +372,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The parent item identifier.
         """
+        return self.model.get_data(item_id, "parent")
 
     def setParentId(self, item_id: str, parent_item_id: str) -> bool:
         """Set the unique identifier for this item’s parent item.
@@ -180,6 +382,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             bool: The success state.
         """
+        return self.model.set_data(item_id, "parent", parent_item_id)
 
     def childItemIds(self, item_id: str) -> tuple[str]:
         """A list of unique identifiers for all assets that
@@ -189,6 +392,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             tuple[str]: The child item ids.
         """
+        return self.model.get_data(item_id, "children")
 
     def prepareItemForUse(self, item_id: str) -> str:
         """Make sure that the item is ready to be used.
@@ -200,6 +404,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: Empty string if item is ready, otherwise error.
         """
+        return ""
 
     def markItemsForDeletion(self, item_ids: tuple[str]) -> bool:
         """Marks one or more items to be deleted.
@@ -208,6 +413,12 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The success state.
         """
+        states = []
+        for item_id in item_ids:
+            states.append(
+                self.model.set_item_mark_deleted(item_id)
+            )
+        return all(states)
 
     def unmarkItemsForDeletion(self, item_ids: tuple[str]) -> bool:
         """Remove the indicator in the data source that the
@@ -217,7 +428,13 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The success state.
         """
-
+        states = []
+        for item_id in item_ids:
+            states.append(
+                self.model.set_item_mark_undeleted(item_id)
+            )
+        return all(states)
+    
     def addItem(
         self,
         label: str,
@@ -238,6 +455,15 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The item identifier.
         """
+        item = Item(
+            id=file_path,
+            label=label,
+            file_path=file_path,
+            thumbnail_file_content=thumbnail,
+            blind_data=blind_data,
+            creation_date=creation_date
+        )
+        self.model.add_item(item)
 
     def filePath(self, item_id: str) -> str:
         """Return a string that can be used to access
@@ -281,6 +507,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The success state.
         """
+        return self.model.add_tag(tag)
 
     def deleteTag(self, tag: str, delete_if_assigned: bool) -> bool:
         """Delete a tag from the data source.
@@ -297,6 +524,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The success state.
         """
+        return self.model.delete_tag(tag, delete_if_assigned)
 
     def addTag(self, item_id, tag) -> bool:
         """Adds a tag to a specific item.
@@ -326,7 +554,7 @@ class AssetLibrary(husd.datasource.DataSource):
             item_id(str): The item id.
             tag(str): The tag name.
         Returns:
-            str: Data source args string.
+            str: The success state.
         """
         tags = self.model.get_data(item_id, "tags")
         if tag in tags:
@@ -345,7 +573,10 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             tuple[str]: The tags.
         """
-        return self.model.get_data(item_id, "tags")
+        item_tags = []
+        if item_id:
+            item_tags = self.model.get_data(item_id, "tags")
+        return tuple(sorted(list(item_tags) + list(self.model.get_tags())))
 
     def status(self, item_id) -> str:
         """Return a string describing the current status of this item.
@@ -402,7 +633,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             str: The thumbnail file path.
         """
-        return self.model.get_data(item_id, "thumbnail")
+        return self.model.get_data(item_id, "thumbnail_file_content")
 
     def setThumbnail(self, item_id: str, thumbnail: str) -> bool:
         """Return the raw data for a thumbnail image that represents the item.
@@ -412,6 +643,7 @@ class AssetLibrary(husd.datasource.DataSource):
         Returns:
             bool: The value changed state.
         """
+        return self.model.set_data(item_id, "thumbnail_file_path", thumbnail)
 
     def ownsFile(self, item_id: str) -> bool:
         """Return True if the filePath for this item is a file on disk
